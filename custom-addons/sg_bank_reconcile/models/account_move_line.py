98ec0382399d5b3eff7f163e99c54dd065782a8d
Vlad J <65186251+vladimir-givex@users.noreply.github.com>
2020-08-21 13:11:39 -0400

    sg_bank_reconcile fixes

--- custom-addons/sg_bank_reconcile/models/account_move_line.py
+++ custom-addons/sg_bank_reconcile/models/account_move_line.py
@@ -7,10 +7,12 @@ class AccountMoveLine(models.Model):
 
     cleared_bank_account = fields.Boolean(
         'Cleared? ',
+        copy=False,
         help='Check if the transaction has cleared from the bank')
     bank_acc_rec_statement_id = fields.Many2one(
         'bank.acc.rec.statement',
         'Bank Acc Rec Statement',
+        copy=False,
         help="The Bank Acc Rec Statement linked with the journal item")
     draft_assigned_to_statement = fields.Boolean(
         'Assigned to Statement? ',
