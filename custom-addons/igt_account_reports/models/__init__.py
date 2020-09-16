4e63f8ef91da7452eb716520c9eabf3a3dfb9cc3
Vlad J <65186251+vladimir-givex@users.noreply.github.com>
2020-09-16 16:06:25 -0400

    Copied igt_account_reports module from June3.0 staging to September.

---
+++ custom-addons/igt_account_reports/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Copyright (C) 2020 (https://ingenieuxtech.odoo.com)
+# ingenieuxtechnologies@gmail.com
+# ingenieuxtechnologies
+#
+##############################################################################
+from . import account_report
+from . import account_aged_receivable
