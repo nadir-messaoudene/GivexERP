5635503d464d74222a97581e685f4e55a4ca88d0
pariket <pariket@syncoria.com>
2023-02-10 19:00:47 +0530

    [FIX]: partner_statement comment configration file

--- custom-addons/partner_statement/__manifest__.py
+++ custom-addons/partner_statement/__manifest__.py
@@ -17,7 +17,7 @@
         "views/outstanding_statement.xml",
         "views/assets.xml",
         "views/aging_buckets.xml",
-        "views/res_config_settings.xml",
+        # "views/res_config_settings.xml",
         "wizard/statement_wizard.xml",
     ],
     "installable": True,
