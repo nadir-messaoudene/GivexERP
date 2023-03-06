54b0d9c95e0d7423c7440f890682458836b2fe46
Toan Truong <toan@syncoria.com>
2023-03-06 16:12:03 -0500

    update

--- custom-addons/report_xlsx/controllers/main.py
+++ custom-addons/report_xlsx/controllers/main.py
@@ -98,10 +98,10 @@ class ReportController(report.ReportController):
             else:
                 return super(ReportController, self).report_download(data, context)
         except Exception as e:
-            if report_name:
-                _logger.exception("Error while generating report %s", reportname)
-            else: 
-                _logger.exception("Error %s", e)
+            # if report_name:
+            #     _logger.exception("Error while generating report %s", reportname)
+            # else: 
+            _logger.exception("Error %s", e)
             se = _serialize_exception(e)
             error = {"code": 200, "message": "Odoo Server Error", "data": se}
             return request.make_response(html_escape(json.dumps(error)))
