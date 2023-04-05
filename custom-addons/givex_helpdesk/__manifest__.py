414666a9a6d78c26676be662965c0a468e81813e
Toan Truong <toan@syncoria.com>
2023-04-05 11:28:58 -0400

    [ADD] givex_helpdesk: reopen stage once customer reply

---
+++ custom-addons/givex_helpdesk/__manifest__.py
@@ -0,0 +1,15 @@
+# $Id: __manifest__.py,v 1.1 2020/12/08 21:31:34 skumar Exp $
+# Copyright Givex Corporation.  All rights reserved.
+{
+    'name': 'Givex Helpdesk',
+    'version': '0.1',
+    'summary': 'CRM',
+    'author': 'Syncoria Inc',
+    'description': """Customizing CRM leads module""",
+    'depends': ['base_setup', 'helpdesk', 'mail'],
+    'data': [
+    ],
+    'demo': [],
+    'installable' : True,
+    'auto_install': False,
+}
---
+++ custom-addons/givex_helpdesk/models/mail_message.py
@@ -0,0 +1,24 @@
+import logging
+from psycopg2 import sql
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, tools, SUPERUSER_ID
+from odoo.exceptions import UserError, AccessError
+from collections import OrderedDict, defaultdict
+
+_logger = logging.getLogger(__name__)
+
+class Message(models.Model):
+    _inherit = 'mail.message'
+
+    @api.model_create_multi
+    def create(self, values_list):
+        mails = super(Message, self).create(values_list)
+        for mail in mails:
+            if mail.model == 'helpdesk.ticket':
+                ticket = self.env['helpdesk.ticket'].search([('id','=',mail.res_id)])
+                if ticket and ticket.stage_id.is_close and ticket.partner_id == mail.author_id:
+                    reopen_stage = self.env['helpdesk.stage'].search([('name','=','Reopen')])
+                    ticket.stage_id = reopen_stage
+        return mails
