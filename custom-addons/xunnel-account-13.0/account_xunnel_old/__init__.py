3f88eb28d152abec5dd24cf00a8e60dee6c44788
pariket <pariket@syncoria.com>
2023-04-05 19:42:41 +0530

    [DELETE]: account_xunnel_old remove module

--- custom-addons/xunnel-account-13.0/account_xunnel_old/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2017, Vauxoo, Jarsa Sistemas, S.A. de C.V.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import models
-from . import controllers
-from . import wizards
--- custom-addons/xunnel-account-13.0/account_xunnel_old/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2017, Vauxoo, Jarsa Sistemas, S.A. de C.V.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from . import account_config_settings
-from . import res_company
-from . import xunnel_provider_account
-from . import xunnel_online_account
-from . import xunnel_account
-from . import account_bank_statement_line
