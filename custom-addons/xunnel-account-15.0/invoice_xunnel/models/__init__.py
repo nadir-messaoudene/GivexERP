70bcbc832adbb563d7f58c8b31bc3814fd58b51b
pariket <pariket@syncoria.com>
2023-04-12 02:04:43 +0530

    [IMP]: improve some code for fixed some issue

--- custom-addons/xunnel-account-15.0/invoice_xunnel/models/__init__.py
+++ custom-addons/xunnel-account-15.0/invoice_xunnel/models/__init__.py
@@ -2,10 +2,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 # from . import documents, documents_workflow_rule, ir_attachment, res_company
--from . import res_company
+from . import res_company
 # -from . import workflow
--from . import documents_workflow_rule
--from . import documents
--from . import account_invoice
--from . import ir_attachment
--from . import res_config_settings
+from . import documents_workflow_rule
+from . import documents
+from . import account_invoice
+from . import ir_attachment
+from . import res_config_settings
