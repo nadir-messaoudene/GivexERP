9c04a7d3c0d19f45bca3a9eb9d717c979235f706
Toan Truong <toan@syncoria.com>
2023-05-23 11:46:14 -0400

    [RMV] os_payment_website

--- custom-addons/os_payment_website/payment_apps/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#    License, author and contributors information in:                         #
-#    __manifest__.py file at the root folder of this module.                  #
-###############################################################################
-
-from . import payment_moneris_checkout
-from . import odoo_bambora_checkout
