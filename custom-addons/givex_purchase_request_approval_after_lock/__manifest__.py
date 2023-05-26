331a1fd596878e2ce5a0296d8b23a52263af962b
nadir-syncoria <nadir@syncoria.com>
2023-05-25 21:39:35 -0700

    [ADD] Enable PO approval after being locked

---
+++ custom-addons/givex_purchase_request_approval_after_lock/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2017 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order request Approved after lock",
+    "summary": "Enable PO approval after being locked",
+    "version": "15.0.1.0.0",
+    "category": "purchase_stock",
+    'category': 'purchase',
+    'description': """This module contains custom modification for Purchase orders allow user send approval request after being locked.""",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["purchase"],
+}
---
+++ custom-addons/givex_purchase_request_approval_after_lock/models/purchase_order.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def write(self, values):
+        new_status = False
+        active_id = None
+
+        if self.env.context.get('active_id'):
+            active_id = self.env.context['active_id']
+        elif len(self.ids) == 1:
+            active_id = self.ids[0]
+        if active_id and 'state' not in values:
+            purchase_order = self.env['purchase.order'].sudo().browse(active_id)
+            curr_state = self.env.cr.execute("SELECT state FROM purchase_order WHERE id = {0}".format(active_id))
+            if curr_state != 'purchase':
+                for order in purchase_order:
+                    if order.state == 'purchase':
+                        new_status = True
+                if new_status:
+                    values['state'] = 'draft'
+                    return super(PurchaseOrder, self).write(values)
+
+        return super(PurchaseOrder, self).write(values)
