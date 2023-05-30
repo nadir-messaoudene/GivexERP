a40670cb47df036b87e85fdfcd16b2dd7b8a7e7d
pariket <pariket@syncoria.com>
2023-05-31 01:00:53 +0530

    {IMP]: set default value of idel time is 0

--- custom-addons/idle_logout/models/res_users.py
+++ custom-addons/idle_logout/models/res_users.py
@@ -5,7 +5,7 @@ from odoo import api, http, models, tools, fields
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    idle_timer = fields.Integer(string="Idle Timer (seconds)", default=7200)
+    idle_timer = fields.Integer(string="Idle Timer (seconds)", default=0)
 
     def idle_times(self):
         print("\n\n\n", self.idle_timer)
