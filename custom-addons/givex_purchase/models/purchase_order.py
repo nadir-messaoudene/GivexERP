061b911b9a3edd05ead1ef5892c14c0ed0d35f41
nadir-syncoria <nadir@syncoria.com>
2023-05-30 20:12:16 -0500

    [1560][ADD] Purchase_order: fix bug in confirmation order

--- custom-addons/givex_purchase/models/purchase_order.py
+++ custom-addons/givex_purchase/models/purchase_order.py
@@ -7,7 +7,7 @@ from odoo import api, fields, models, _
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    editing = fields.Boolean(default=False)
+    editing = fields.Boolean(default=True, dtore=True)
 
     @api.onchange('order_line', 'order_line.price_unit', 'order_line.product_qty')
     def onchangeOrderLine(self):
@@ -20,18 +20,16 @@ class PurchaseOrder(models.Model):
     def write(self, values):
         new_status = False
         active_id = None
-
         if self.env.context.get('active_id'):
             active_id = self.env.context['active_id']
         elif len(self.ids) == 1:
             active_id = self.ids[0]
         if active_id and 'state' not in values:
             purchase_order = self.env['purchase.order'].sudo().browse(active_id)
-            curr_state = self.env.cr.execute("SELECT state FROM purchase_order WHERE id = {0}".format(active_id))
-            if curr_state != 'purchase':
-                for order in purchase_order:
-                    if order.state == 'purchase' and self.editing == True:
-                        new_status = True
-                if new_status:
-                    values['state'] = 'draft'
+            for order in purchase_order:
+                if order.state == 'purchase':
+                    editing = order.editing
+                    new_status = True
+            if new_status and 'editing' in values:
+                values['state'] = 'draft'
         return super(PurchaseOrder, self).write(values)
